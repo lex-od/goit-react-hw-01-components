b8d2079d3fc7f31b11f4ad4b2e825bf37e26f11a
Aleksey Yesin <esinru@gmail.com>
2021-04-02 16:41:24 +0300

    FriendList key using optimized

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import FriendListItem from '../FriendListItem/FriendListItem';
 import css from './FriendList.module.css';
@@ -6,9 +5,7 @@ import css from './FriendList.module.css';
 const FriendList = ({ friends }) => (
     <ul className={css['friend-list']}>
         {friends.map(({ id, ...rest }) => (
-            <Fragment key={id}>
-                <FriendListItem {...rest} />
-            </Fragment>
+            <FriendListItem key={id} {...rest} />
         ))}
     </ul>
 );
